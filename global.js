13bd82cc08e1fa91cb75dccfa529b312b31c58c5
Yi-Hsuan Kuo <y3kuo@ucsd.edu>
2025-04-16 20:52:00 -0700

    step 3

--- global.js
+++ global.js
@@ -3,3 +3,43 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+
+//   currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/YihsuanKuo', title: 'Github Portfolio' },
+    { url: 'cv/', title: 'My CV' }
+  ];
+  
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/website/"; // Replace with your GitHub repo name
+  
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+  
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    if (a.host !== location.host){
+        a.target = '_blank';
+    }
+    nav.append(a);
+    
+  }

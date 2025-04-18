ad01a21e38f274517403889e2a6d15727ee20f03
Yi-Hsuan Kuo <y3kuo@ucsd.edu>
2025-04-18 12:30:11 -0700

    again

--- global.js
+++ global.js
@@ -22,7 +22,7 @@ let pages = [
   
   const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
-  : "yihsuankuo.github.io/portfolio/";         // GitHub Pages repo name
+  : "https://yihsuankuo.github.io/portfolio/";         // GitHub Pages repo name
   
 let nav = document.createElement('nav');
 document.body.prepend(nav);
